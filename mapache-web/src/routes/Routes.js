69ac559bd8fdfa4d7530ebdc339fa6f855cc6664
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-06-21 20:52:15 -0300

    S5-26 Agregar NavBar a Routes

--- mapache-web/src/routes/Routes.js
+++ mapache-web/src/routes/Routes.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Home } from "../controllers/Home";
+import NavBar from "../component/NavBar";
+import { Home } from "../controller/Home";
 
 class Routes extends Component {
     render() {
         return (
             <Router key="router">
+                <NavBar />
                 <Route exact path={"/"} component={Home} />
             </Router>
         )

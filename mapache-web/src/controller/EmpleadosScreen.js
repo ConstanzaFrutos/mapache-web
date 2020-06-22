f19e4ed3a125d3209f76c6bc8c0b633df5e647ed
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-06-21 21:01:34 -0300

    S5-26 Pantalla de empleados

---
+++ mapache-web/src/controller/EmpleadosScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+
+class EmpleadosScreen extends Component {
+
+    render() {
+        return (
+            <div className="empleados-screen-div">
+
+            </div>
+        )
+    }
+
+}
+
+export default withRouter(EmpleadosScreen);

bb536e31b317a1da331c8a0d1f8c7d57704ebc6c
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-06-21 21:01:20 -0300

    S5-26 Mapeo desde Routes a las otras pantallas

--- mapache-web/src/routes/Routes.js
+++ mapache-web/src/routes/Routes.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "../component/NavBar";
 import { Home } from "../controller/Home";
+import EmpleadosScreen from "../controller/EmpleadosScreen";
+import SoporteScreen from "../controller/SoporteScreen";
+import ProyectosScreen from "../controller/ProyectosScreen";
 
 class Routes extends Component {
     render() {
@@ -9,6 +12,9 @@ class Routes extends Component {
             <Router key="router">
                 <NavBar />
                 <Route exact path={"/"} component={Home} />
+                <Route exact path={"/empleados"} component={ EmpleadosScreen }/>
+                <Route exact path={"/proyectos"} component={ ProyectosScreen }/>
+                <Route exact path={"/soporte"} component={ SoporteScreen }/>
             </Router>
         )
     }

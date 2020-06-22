298a2994fbd4c098a917ad05e1a859e94ec5992d
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-06-21 21:01:51 -0300

    S5-26 Pantalla de soporte

---
+++ mapache-web/src/controller/SoporteScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+
+class SoporteScreen extends Component {
+
+    render() {
+        return (
+            <div className="soporte-screen-div">
+
+            </div>
+        )
+    }
+
+}
+
+export default withRouter(SoporteScreen);

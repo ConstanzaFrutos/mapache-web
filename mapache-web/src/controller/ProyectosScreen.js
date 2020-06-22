58f91688e8c12b51891cdd989d8416499c7a54be
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-06-21 21:01:45 -0300

    S5-26 Pantalla de proyectos

---
+++ mapache-web/src/controller/ProyectosScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+
+class ProyectosScreen extends Component {
+
+    render() {
+        return (
+            <div className="proyectos-screen-div">
+
+            </div>
+        )
+    }
+
+}
+
+export default withRouter(ProyectosScreen);

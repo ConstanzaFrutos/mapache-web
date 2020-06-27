48886b52958515fe17919c0656bc303dcad8d258
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-06-27 12:41:27 -0300

    S5-26 Remuevo imports sin usar para evitar warnings

--- mapache-web/src/component/NavBar.js
+++ mapache-web/src/component/NavBar.js
@@ -6,7 +6,7 @@ class NavBar extends Component {
     render() {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark">
-                <a className="navbar-brand" href="#">PSA</a>
+                <a className="navbar-brand" href="/">PSA</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
--- mapache-web/src/controller/Home.js
+++ mapache-web/src/controller/Home.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 import "../assets/css/Home.css";
 import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
 export class Home extends Component {

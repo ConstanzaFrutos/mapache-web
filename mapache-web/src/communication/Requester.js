039dde25e1522f2c5fb980da86ec502b2a5c036d
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-01 18:47:39 -0300

    SIDG-58 Agrego definicion de la clase de comunicacion para enviar requests

---
+++ mapache-web/src/communication/Requester.js
@@ -0,0 +1,59 @@
+class Requester {
+    constructor(baseUrl){
+        this.baseUrl = baseUrl;
+
+        this.get = this.get.bind(this);
+    }
+    
+    get (url, callback) {
+        let response = fetch(this.baseUrl + url, {
+                            method: 'GET',
+                            headers: this.getHeaders()
+                        })
+        return response;
+    }
+
+    post (url, payload, callback) {
+        let response = fetch(this.baseUrl + url, {
+                            method: 'POST',
+                            headers: this.getHeaders(),
+                            body: JSON.stringify(payload)
+                        })
+                        .then(resp =>{
+                            console.log(resp);
+                            return resp;
+                        })
+        return response;
+    }
+
+    put (url, payload, callback) {
+        let response = fetch(this.baseUrl + url, {
+            method: 'PUT',
+            headers: this.getHeaders(),
+            body: JSON.stringify(payload)
+        })
+        return response;
+    }
+
+    delete (url, payload, callback) {
+        let response = fetch(this.baseUrl + url, {
+            method: 'DELETE',
+            headers: this.getHeaders(),
+            body: JSON.stringify(payload)
+        })
+        return response;
+    }
+
+    getHeaders () {
+        let headers = {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache'
+        }
+        
+        return headers;
+    }
+
+}
+
+export default Requester;

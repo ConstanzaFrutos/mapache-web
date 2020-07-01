e43b2e9d1f11cdf1f020233f2ea53f50f86cf88c
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-01 19:14:57 -0300

    SIDG-58 Refactor de la tabla de empleados

--- mapache-web/src/controller/EmpleadosScreen.js
+++ mapache-web/src/controller/EmpleadosScreen.js
@@ -8,9 +8,44 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
 import TableBody from "@material-ui/core/TableBody";
+
+import Requester from "../communication/Requester";
+
 import "../assets/css/controller/EmpleadosScreen.css";
 
+//const mapacheRecursosBaseUrl = "https://mapache-recursos.herokuapp.com/";
+const mapacheRecursosBaseUrl = "http://0.0.0.0:8080";
+
 class EmpleadosScreen extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.requester = new Requester(mapacheRecursosBaseUrl);
+
+        this.state = {
+            empleados: []
+        }
+    }
+
+    componentDidMount() {
+        this.requester.get('/empleados/')
+        .then(response => {
+            if (response.ok){
+                return response.json();
+            } else {
+                console.log("Error al consultar empleados");
+            }
+        })
+        .then(response => {
+            console.log(response);
+            if (response) {
+                this.setState({
+                    empleados: response
+                });
+            }
+        });
+    }
 
     render() {
         return (
@@ -20,21 +55,28 @@ class EmpleadosScreen extends Component {
                         <Table aria-label="simple table">
                             <TableHead>
                                 <TableRow>
-                                    <TableCell>Empleado</TableCell>
-                                    <TableCell align="right">Seniority</TableCell>
-                                    <TableCell align="right">Contrato</TableCell>
-                                    <TableCell align="right">Horas trabajadas</TableCell>
+                                    <TableCell>{title}</TableCell>
+                                    {tableCells.map((cell) => {
+                                        if (cell != "legajo")
+                                            return <TableCell align="right">{cell}</TableCell>
+                                    })}
                                 </TableRow>
                             </TableHead>
                             <TableBody>
                                 {rows.map((row) => (
                                     <TableRow key={row.nombre}>
                                         <TableCell component="th" scope="row">
-                                            {row.nombre}
+                                            {row.legajo}
                                         </TableCell>
-                                        <TableCell align="right">{row.seniority}</TableCell>
-                                        <TableCell align="right">{row.contrato}</TableCell>
-                                        <TableCell align="right">{row.horasTrabajadas}</TableCell>
+
+                                        {tableCells.map((cell) => {
+                                            console.log(cell);
+                                            if (cell != "legajo")
+                                                return <TableCell align="right">
+                                                            {row[cell]}
+                                                        </TableCell>
+                                        })}
+                                        
                                     </TableRow>
                                 ))}
                             </TableBody>
@@ -49,13 +91,40 @@ class EmpleadosScreen extends Component {
 
 export default withRouter(EmpleadosScreen);
 
-function createData(nombre, seniority, contrato, horasTrabajadas) {
-    return { nombre, seniority, contrato, horasTrabajadas };
+const title = "Empleado";
+
+const tableCells = [
+    "legajo",
+    "nombre",
+    "apellido", 
+    "contrato",
+    "seniority"
+]
+
+function createData(legajo, nombre, apellido, contrato, seniority) {
+    return { legajo, nombre, apellido, contrato, seniority };
 }
 
 const rows = [
-    createData('Jane Doe', "Senior", "Full-time", "19 hs"),
-    createData('Jon Snow', "Junior", "Soporte", "12 hs"),
-    createData('Hermione Granger', "Senior", "Full-time", "40 hs"),
-    createData('Anakin Skywalker', "Senior", "Part-time", "4 hs"),
+    createData('1','Jane', 'Doe', "Full-time", "Senior"),
+    createData('2', 'Jon', 'Snow', "Junior", "Soporte"),
+    createData('3', 'Hermione', 'Granger', "Senior", "Full-time"),
+    createData('4', 'Anakin', 'Skywalker', "Senior", "Part-time"),
 ];
+
+const empleados = [
+    {
+        legajo: "1",
+        nombre: "nombre",
+        apellido: "apellido",
+        seniority: "junior",
+        contrato: "full-time"
+    },
+    {
+        legajo: "2",
+        nombre: "nombre2",
+        apellido: "apellido2",
+        seniority: "junior",
+        contrato: "full-time"
+    }
+]

5dc1d36536015daf6f00574b0a68ff5196c5ce3e
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-02 21:06:05 -0300

    SIDG-58 Implementacion del componente tabla

---
+++ mapache-web/src/component/Tabla.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+import TableContainer from "@material-ui/core/TableContainer";
+import Paper from "@material-ui/core/Paper";
+import Table from "@material-ui/core/Table";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
+
+import "../assets/css/component/Tabla.css";
+
+export class Tabla extends Component {
+    
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="tabla">
+                <TableContainer component={Paper}>
+                    <Table aria-label="simple table">
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>{this.props.title}</TableCell>
+                                {this.props.tableCells.map((cell) => {
+                                    if (cell !== this.props.tableCells[0])
+                                        return <TableCell align="right">{cell}</TableCell>
+                                })}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {this.props.rows.map((row) => (
+                                <TableRow key={row.nombre}>
+                                    <TableCell component="th" scope="row">
+                                        {row.legajo}
+                                    </TableCell>
+
+                                    {this.props.tableCells.map((cell) => {
+                                        if (cell !== this.props.tableCells[0])
+                                            return <TableCell align="right">
+                                                        {row[cell]}
+                                                    </TableCell>
+                                    })}
+                                    
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+        )
+    }
+
+}

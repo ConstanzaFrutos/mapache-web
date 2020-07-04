b620c3d7e877c31365b9733f95e0a8a8981b9047
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-03 23:37:58 -0300

    SIDG-59 Uso tabla con funcionalidades extrade ejemplo

--- mapache-web/src/controller/EmpleadosScreen.js
+++ mapache-web/src/controller/EmpleadosScreen.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
 
-import { Tabla } from "../component/Tabla";
+import { TablaAdministracion } from "../component/general/TablaAdministracion";
+import { Tabla } from "../component/general/Tabla";
 
 import Requester from "../communication/Requester";
 
 import "../assets/css/controller/EmpleadosScreen.css";
 
-const mapacheRecursosBaseUrl = "https://mapache-recursos.herokuapp.com";
-//const mapacheRecursosBaseUrl = "http://0.0.0.0:8080";
+//const mapacheRecursosBaseUrl = "https://mapache-recursos.herokuapp.com";
+const mapacheRecursosBaseUrl = "http://0.0.0.0:8080";
 
 class EmpleadosScreen extends Component {
 
@@ -18,11 +19,64 @@ class EmpleadosScreen extends Component {
         this.requester = new Requester(mapacheRecursosBaseUrl);
 
         this.state = {
-            empleados: [],
-            rows: []
+            empleados: []
         }
 
-        this.createRows = this.createRows.bind(this);
+        
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleEdit = this.handleEdit.bind(this);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    handleAdd(newData) {
+        console.log(newData);
+        let empleado = {
+            "activo": true,
+            "apellido": newData.apellido,
+            "contrato": newData.contrato,
+            "dni": "",
+            "fechaNacimiento": "1990-06-02",
+            "legajo": newData.legajo,
+            "nombre": newData.nombre,
+            "proyectos": [
+              
+            ],
+            "rol": "DESARROLLADOR",
+            "seniority": newData.seniority
+        }
+
+        this.requester.post('/empleados/', empleado)
+            .then(response => {
+                if (response.ok){
+                    return response.json();
+                } else {
+                    console.log("Error al consultar empleados");
+                }
+            });
+    }
+
+    handleEdit(newData, oldData) {
+        console.log("En edit");
+        /*this.requester.put('/empleados/' + oldData.legajo)
+        .then(response => {
+            if (response.ok){
+                return response.json();
+            } else {
+                console.log("Error al consultar empleados");
+            }
+        });*/
+    }
+
+    handleDelete(newData) {
+        console.log("En delete");
+        this.requester.delete('/empleados/' + newData.legajo)
+        .then(response => {
+            if (response.ok){
+                console.log(`Empleado ${newData.legajo} fue eliminado de manera exitosa`);
+            } else {
+                console.log("Error al consultar empleados");
+            }
+        });
     }
 
     componentDidMount() {
@@ -36,37 +90,25 @@ class EmpleadosScreen extends Component {
         })
         .then(response => {
             console.log(response);
-            let rows = this.createRows(response);
             if (response) {
                 this.setState({
-                    empleados: response,
-                    rows: rows
+                    empleados: response
                 });
             }
-        });
-    }
-
-    createRows(empleados) {
-        //legajo, nombre, apellido, contrato, seniority
-        return empleados.map( (empleado) => {
-            return createData(
-                empleado.legajo, 
-                empleado.nombre,
-                empleado.apellido, 
-                empleado.contrato,
-                empleado.seniority
-            );
         });
     }
 
     render() {
         return (
             <div className="empleados-screen-div">
-                <Tabla 
-                    title = { title }
-                    tableCells={ tableCells }
-                    rows={ this.state.rows }
-                ></Tabla>
+                <TablaAdministracion 
+                    title={ title }
+                    columns={ columns }
+                    data={ this.state.empleados }
+                    handleAdd={ this.handleAdd }
+                    handleEdit={ this.handleEdit }
+                    handleDelete={ this.handleDelete } 
+                ></TablaAdministracion>
             </div>
         )
     }
@@ -75,16 +117,28 @@ class EmpleadosScreen extends Component {
 
 export default withRouter(EmpleadosScreen);
 
-const title = "Empleado";
+const title = "Empleados";
 
-const tableCells = [
-    "Legajo",
-    "Nombre",
-    "Apellido", 
-    "Contrato",
-    "Seniority"
+const columns = [
+    {
+        title: "Legajo", 
+        field: "legajo"
+    },
+    {
+        title: "Nombre", 
+        field: "nombre"
+    },
+    {
+        title: "Apellido", 
+        field: "apellido"
+    },
+    {
+        title: "Contrato", 
+        field: "contrato"
+    },
+    {
+        title: "Seniority", 
+        field: "seniority"
+    }
 ]
 
-function createData(legajo, nombre, apellido, contrato, seniority) {
-    return { legajo, nombre, apellido, contrato, seniority };
-}

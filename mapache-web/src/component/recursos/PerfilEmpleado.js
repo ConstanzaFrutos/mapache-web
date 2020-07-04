5c437a5d2002364c6312f57b92ff4a49d48ce6e3
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-04 15:37:25 -0300

    SIDG-59 Implementacion del perfil empleado

--- mapache-web/src/component/recursos/PerfilEmpleado.js
+++ mapache-web/src/component/recursos/PerfilEmpleado.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
 
+import "../../assets/css/component/recursos/PerfilEmpleado.css";
+
+import Avatar from '@material-ui/core/Avatar';
+import PhoneIcon from '@material-ui/icons/Phone';
+
+import Paper from '@material-ui/core/Paper';
+import { TabPanel } from "../general/TabPanel";
+
 import Requester from "../../communication/Requester";
 
-//const mapacheRecursosBaseUrl = "https://mapache-recursos.herokuapp.com";
-const mapacheRecursosBaseUrl = "http://0.0.0.0:8080";
+const mapacheRecursosBaseUrl = "https://mapache-recursos.herokuapp.com";
+//const mapacheRecursosBaseUrl = "http://0.0.0.0:8080";
 
 class PerfilEmpleado extends Component {
 
@@ -14,9 +22,12 @@ class PerfilEmpleado extends Component {
         this.requester = new Requester(mapacheRecursosBaseUrl);
 
         this.state = {
-            empleado: {}
+            empleado: {},
+            iniciales: "",
+            fechaNacimientoFormateada: ""
         }
 
+        this.formatearFecha = this.formatearFecha.bind(this);
     }
 
     componentDidMount() {
@@ -33,18 +44,70 @@ class PerfilEmpleado extends Component {
             })
             .then(response => {
                 console.log(response);
+                let iniciales = response.nombre.charAt(0) + response.apellido.charAt(0);
+                iniciales = iniciales.toUpperCase();
+                let fechaNacimientoFormateada = this.formatearFecha(response.fechaNacimiento);
+                console.log(fechaNacimientoFormateada);
                 if (response) {
                     this.setState({
-                        empleado: response
+                        empleado: response,
+                        iniciales: iniciales,
+                        fechaNacimientoFormateada: fechaNacimientoFormateada
                     });
                 }
             });
     }
 
+    formatearFecha(fecha) {
+        let año = fecha[0];
+        let mes = fecha[1];
+        let dia = fecha[2];
+
+        return `${dia}/${mes}/${año}`;
+    }
+
     render() {
+
+        let value = [
+            "Información",
+            "Carga de horas"
+        ]
+
+        let labels = [
+            "Información",
+            "Carga de horas"
+        ]
+
+        let icons = [
+            <PhoneIcon></PhoneIcon>,
+            <PhoneIcon></PhoneIcon>
+        ]
+
         return (
             <div className="perfil-empleado">
-                {this.state.empleado.legajo}
+                <div className={"foto-y-nombre"}>
+                    <Avatar className="avatar">{this.state.iniciales}</Avatar>
+                    <p className={"nombre"}>{this.state.empleado.apellido + ", " + this.state.empleado.nombre}</p>
+                </div>
+                <div className={ "perfil-empleado-body" }>
+                    <Paper square className="paper-informacion">
+                        <div className="informacion">
+                            <p>Nombre y Apellido: { this.state.empleado.nombre + ", " + this.state.empleado.apellido }</p>
+                            <p>DNI: { this.state.empleado.dni }</p>
+                            <p>Fecha de nacimiento: { this.state.fechaNacimientoFormateada }</p>
+                            <br></br>
+                            <p>Legajo: { this.state.empleado.legajo }</p>
+                            <br></br>
+                            <p>Seniority: { this.state.empleado.seniority }</p>
+                            <br></br>
+                            <p>Contrato: { this.state.empleado.contrato }</p>
+                            <br></br>
+                            <p>Antigüedad: { this.state.empleado.antiguedad }</p>
+                            <br></br>
+                            <p>Rol: { this.state.empleado.rol }</p>
+                        </div>
+                    </Paper>
+                </div>
             </div>
         )
     }

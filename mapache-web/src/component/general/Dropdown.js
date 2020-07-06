f478d7f93fb0b72ab5f013e9f44114679798f698
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-06 20:06:54 -0300

    SIDG-54 Permito la opcion de mostrarlo o no

--- mapache-web/src/component/general/Dropdown.js
+++ mapache-web/src/component/general/Dropdown.js
@@ -10,29 +10,34 @@ import "../../assets/css/component/general/Dropdown.css";
 export class Dropdown extends Component {
 
     render() {
+        let page = null;
+
+        if (this.props.renderDropdown) {
+            page =  <FormControl className="form-control">
+                        <InputLabel id="demo-simple-select-label">
+                            { this.props.label}
+                        </InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            value={ this.props.value }
+                            onChange={ this.props.handleChange }
+                        >
+                            { 
+                                this.props.values.map((value) => {
+                                    return <MenuItem value={ value }
+                                            >
+                                                { value }
+                                            </MenuItem>
+                                }) 
+                            }
+                        </Select>
+                    </FormControl>
+        }
 
         return (
             <div className="dropdown">
-                <FormControl className="form-control">
-                    <InputLabel id="demo-simple-select-label">
-                        { this.props.label}
-                    </InputLabel>
-                    <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                        value={ this.props.value }
-                        onChange={ this.props.handleChange }
-                    >
-                        { 
-                            this.props.values.map((value) => {
-                                return <MenuItem value={ value }
-                                        >
-                                            { value }
-                                        </MenuItem>
-                            }) 
-                        }
-                    </Select>
-                </FormControl>
+                { page }
             </div>
         )
     }

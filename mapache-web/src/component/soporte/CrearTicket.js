ed1efb7cc2ac4032a60f17a5a88b9e17baa4c2a3
Gaston Parente <ggparente95@gmail.com>
2020-07-06 21:36:25 -0300

    estilos

--- mapache-web/src/component/soporte/CrearTicket.js
+++ mapache-web/src/component/soporte/CrearTicket.js
@@ -1,26 +1,56 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
-
-
-
-import "../../assets/css/component/recursos/PerfilEmpleado.css";
-
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
 import Requester from "../../communication/Requester";
 
+import "../../assets/css/component/soporte/Ticket.css";
 
 //const mapacheRecursosBaseUrl = "https://mapache-recursos.herokuapp.com";
 const mapacheSoporteBaseUrl = "http://localhost:5000";
 
+const tipos = [
+  {
+    value: 'Error',
+    label: 'Error'
+  },
+  {
+    value: 'Consulta',
+    label: 'Consulta'
+  },
+  {
+    value: 'Mejora',
+    label: 'Mejora'
+  }
+]
+
+const severidades = [
+  {
+    value: 'Alta',
+    label: 'Alta'
+  },
+  {
+    value: 'Media',
+    label: 'Media'
+  },
+  {
+    value: 'Baja',
+    label: 'Baja'
+  }
+]
+
 
 class CrearTicket extends Component {
+  
   constructor(props){
     super(props);
 
     this.requester = new Requester(mapacheSoporteBaseUrl);
     this.state={
       nombre:'',
-      tipo:'',
-      severidad:'',
+      tipo:'Error',
+      severidad:'Baja',
       descripcion:''
     }
   }
@@ -62,32 +92,48 @@ class CrearTicket extends Component {
     });
   }
 
-render() {
-    return (
-
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>
-            Nombre:
-            <input type="text" name="nombre" onChange={this.handleChangeNombre} />
-          </label>
-          <br />
+/*
           <label>
             tipo:
             <input type="text" name="tipo" onChange={this.handleChangeTipo} />
           </label>
+*/
+
+render() {
+    return (  
+      <div class='form-crear-ticket'>
+        <form noValidate autoComplete="off" onSubmit={this.handleSubmit}>
+          <div>
+            <TextField id="nombre" variant="outlined" name="nombre" label="Nombre" onChange={this.handleChangeNombre}/>
+          </div>
+          <br/>
+          <div>
+            <TextField id="tipo" name="tipo" variant="outlined" select label="Tipo" value={this.state.tipo} onChange={this.handleChangeTipo}>
+            {tipos.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          </div>
+          <br/>
+          <div>
+            <TextField id="severidad" name="severidad" variant="outlined" select label="Severidad" value={this.state.severidad} onChange={this.handleChangeSeveridad}>
+            {severidades.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+            </TextField>
+          </div>
           <br />
-          <label>
-            severidad:
-            <input type="text" name="severidad" onChange={this.handleChangeSeveridad} />
-          </label>
+          <div>
+            <TextField id="descripcion" style={{width: '600px'}} name="descripcion" label="Descripcion" multiline rows={8} variant="outlined" onChange={this.handleChangeDescripcion}/>
+          </div>
           <br />
-          <label>
-            descripcion:
-            <input type="text" name="descripcion" onChange={this.handleChangeDescripcion} />
-          </label>
-          <br />
-          <button type="submit">Add</button>
+          <Button variant="contained" color="primary" type="submit">
+            Agregar
+          </Button>
         </form>
       </div>
     );

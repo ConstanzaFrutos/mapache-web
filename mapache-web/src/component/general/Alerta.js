3351235179a273d03951299f3821aa799be42db4
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-19 11:47:17 -0300

    SIDG-56 Agrego funcionalidad de cerrar alerta

--- mapache-web/src/component/general/Alerta.js
+++ mapache-web/src/component/general/Alerta.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import Alert from '@material-ui/lab/Alert';
 
 export class Alerta extends Component {
-    
+
     render() {
+        let open = this.props.open;
+
         return (
             <div>
-                <Alert variant="outlined" severity={ this.props.tipo }>
-                    { this.props.mensaje }
-                </Alert>
+                <Collapse in={ open }>
+                    <Alert 
+                        variant="outlined" 
+                        severity={ this.props.tipo }
+                        action={
+                                <IconButton
+                                    aria-label="close"
+                                    color="inherit"
+                                    size="small"
+                                    onClick={this.props.handleClose}
+                                >
+                                    <CloseIcon fontSize="inherit" />
+                                </IconButton>
+                            }
+                    >
+                        { this.props.mensaje }
+                    </Alert>
+                </Collapse>
             </div>
         )
     }

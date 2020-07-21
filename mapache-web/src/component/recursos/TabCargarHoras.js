ccd7b96f41e7fbf316ef91504e3e333e116f34a5
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-21 15:37:53 -0300

    SIDG-4 Se agrega el componente TabCargarHoras para realizar la carga de horas

---
+++ mapache-web/src/component/recursos/TabCargarHoras.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+
+class TabCargarHoras extends Component {
+
+    render() {
+        return(
+            <div className="tab-cargar-horas-div">
+                CARGAR HORAS
+            </div>
+        )
+    }
+
+}
+
+export default withRouter(EmpleadosScreen);

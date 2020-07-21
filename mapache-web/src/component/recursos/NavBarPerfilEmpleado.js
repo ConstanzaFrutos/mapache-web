117fb075248029d8a3741dba0da9e31bfc023729
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-21 16:58:23 -0300

    SIDG-4 Se pasan los parametros correspondientes en cada caso

--- mapache-web/src/component/recursos/NavBarPerfilEmpleado.js
+++ mapache-web/src/component/recursos/NavBarPerfilEmpleado.js
@@ -7,9 +7,28 @@ export default class NavBarProyecto extends Component {
         return(
             <Navbar className="navbar navbar-expand-lg navbar-dark">
                 <Nav className="mr-auto">
-                    <Link to={"/empleados/:legajo/proyectos"} className="nav-link">Proyectos</Link>
+                    <Link 
+                        to={{
+                            pathname: `/empleados/${this.props.legajo}`, 
+                            state: { modo: "info", tab: "informacion" }
+                        }} 
+                        params={{ legajo: this.props.legajo }}
+                        className="nav-link"
+                    >Información</Link>
+                    <Link 
+                        to={{
+                            pathname: `/empleados/${this.props.legajo}/proyectos`, 
+                        }} 
+                        className="nav-link"
+                    >Proyectos</Link>
                     <Link to={"/empleados/:legajo/tareas"} className="nav-link">Tareas</Link>
-                    <Link to={"/empleados/:legajo/cargar-horas"} className="nav-link">Cargar Horas</Link>
+                    <Link 
+                        to={{
+                            pathname: `/empleados/${this.props.legajo}`, 
+                            state: { tab: "cargar-horas"}
+                        }} 
+                        className="nav-link"
+                    >Cargar Horas</Link>
                     <Link to={"/empleados/:legajo/horas-cargadas"} className="nav-link">Horas cargadas</Link>
                 </Nav>
             </Navbar>

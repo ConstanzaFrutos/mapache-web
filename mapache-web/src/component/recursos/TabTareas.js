38283e7626f6a69adc62978752ad80bc00626eb3
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-22 20:42:05 -0300

    SIDG-4 Implementacion de la tabla de tareas del empleado para cargar sus horas

--- mapache-web/src/component/recursos/TabTareas.js
+++ mapache-web/src/component/recursos/TabTareas.js
@@ -1,11 +1,82 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+
+import AccessTime from '@material-ui/icons/AccessTime';
+
+import { TablaAdministracion } from "../general/TablaAdministracion";
+
+import Requester from "../../communication/Requester";
+
+const mapacheRecursosBaseUrl = "https://mapache-recursos.herokuapp.com";
+//const mapacheRecursosBaseUrl = "http://0.0.0.0:8080";
 
 class TabTareas extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.requester = new Requester(mapacheRecursosBaseUrl);
+
+        this.state = {
+            empleado: {},
+            proyectos: {},
+            tareas: {}
+        }
+
+        this.handleCargaHoras = this.handleCargaHoras.bind(this);
+    }
+
+    componentDidMount() {
+        let legajo = this.props.match.params.legajo;
+        this.requester.get('/empleados/' + legajo)
+            .then(response => {
+                if (response.ok){
+                    return response.json();
+                } else {
+                    console.log("Error al consultar empleado con legajo: ");
+                }
+            })
+            .then(response => {
+                console.log("Empleado: ", response);
+                let iniciales = response.nombre.charAt(0) + response.apellido.charAt(0);
+                iniciales = iniciales.toUpperCase();                
+
+                if (response) {
+                    this.setState({
+                        empleado: response,
+                        iniciales: iniciales
+                    });
+                }
+            });
+    }
+
+    handleCargaHoras(tarea) {
+        console.log("Tarea a la cual cargar horas: ", tarea);
+    }
 
     render() {
         return (
             <div className="tab-tareas-div">
-
+                <TablaAdministracion
+                    title={ title }
+                    columns={ columns }
+                    data={ data }
+                    handleAdd={ this.handleAdd }
+                    handleEdit={ this.handleEdit }
+                    handleDelete={ this.handleDelete } 
+                    editIcon={ AccessTime }
+                    editable ={ null }
+                    actions={[
+                        {
+                            icon: AccessTime,
+                            tooltip: "Cargar horas",
+                            onClick: (event, rowData) => {
+                                this.handleCargaHoras(rowData)  
+                            }
+                        }
+                    ]}
+                >
+                </TablaAdministracion>
             </div>
         )
     }
@@ -13,3 +84,33 @@ class TabTareas extends Component {
 }
 
 export default withRouter(TabTareas);
+
+const title = `Tareas del empleado`;
+
+const columns = [
+    {
+        title: "Nombre", 
+        field: "nombre"
+    },
+    {
+        title: "Proyecto", 
+        field: "proyecto"
+    },
+    {
+        title: "Horas", 
+        field: "horas"
+    },
+    {
+        title: "Estado", 
+        field: "estado"
+    }
+]
+
+const data = [
+    {
+        nombre: "Tarea 1",
+        proyecto: "Proyecto 1",
+        horas: 20,
+        estado: "En curso"
+    }
+]

bfe01a81af4b868764a63884f68603bccfea453e
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-23 15:30:58 -0300

    SIDG-4 Se agrega la pestaña de proyectos de un empleado

---
+++ mapache-web/src/component/recursos/TabProyectos.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+
+class TabProyectos extends Component {
+
+    render() {
+        return (
+            <div className="tab-proyectos-div">
+                
+            </div>
+        )
+    }
+
+}
+
+export default withRouter(TabProyectos);

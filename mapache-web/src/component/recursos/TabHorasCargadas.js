e74ba0019162cb28ec8bbe040fd2bedd47be819b
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-29 13:13:37 -0300

    SIDG-66 Se agrega la pestaña de horas cargadas para la visualizacion

---
+++ mapache-web/src/component/recursos/TabHorasCargadas.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+
+class TabHorasCargadas extends Component {
+
+    render() {
+        return (
+            <div className="tab-horas-cargadas-div">
+
+            </div>
+        )
+    }
+
+}
+
+export default withRouter(TabHorasCargadas);

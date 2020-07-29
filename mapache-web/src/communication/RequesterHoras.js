b220fbf0f4c740a8cc9438682d76f373481545ad
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-07-29 19:32:12 -0300

    SIDG-66 Implementacion de llamada a la API de recursos para obtener las horas de la semana

--- mapache-web/src/communication/RequesterHoras.js
+++ mapache-web/src/communication/RequesterHoras.js
@@ -7,6 +7,7 @@ class RequesterHoras {
         this.requester = new Requester(mapacheRecursosBaseUrl);
 
         this.obtenerHorasCargadas = this.obtenerHorasCargadas.bind(this);
+        this.obtenerHorasCargadasSemana = this.obtenerHorasCargadasSemana.bind(this);
     }
 
     async obtenerHorasCargadas(legajo){
@@ -16,6 +17,34 @@ class RequesterHoras {
                 return response.json();
             } else {
                 alert.log(`Error al consultar horas del empleado ${legajo}`);
+            }
+        }).then(response => {
+            console.log("response ",response);
+            if (response) {
+                return response;
+            }
+        });
+        return horasCargadas;
+    }
+
+    procesarFecha(fecha) {
+        return `${fecha.getFullYear()}-0${fecha.getMonth() + 1}-${fecha.getDate()}`;
+    }
+
+    async obtenerHorasCargadasSemana(legajo, fechaFin){
+        let fechaInicio = new Date();
+        const nuevoDia = fechaFin.getDate() - 7;
+        fechaInicio.setDate(nuevoDia);
+
+        const fechaIniFormateada = this.procesarFecha(fechaInicio);
+        const fechaFinFormateada = this.procesarFecha(fechaFin);
+        
+        let horasCargadas = await this.requester.get(`/empleados/${legajo}/horas?fechaFin=${fechaFinFormateada}&fechaInicio=${fechaIniFormateada}`)
+        .then(response => {
+            if (response.ok){
+                return response.json();
+            } else {
+                alert(`Error al consultar horas del empleado ${legajo}`);
             }
         }).then(response => {
             console.log("response ",response);

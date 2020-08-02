291c45c898e602f75f840dd329002cbad4a0f56a
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-08-02 20:18:26 -0300

    SIDG-73 se agrega la pestaña de estadisticas

---
+++ mapache-web/src/component/recursos/TabEstadisticas.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+
+class TabEstadisticas extends Component {
+
+    render() {
+        return (
+            <div className="tab-estadisticas-div">
+
+            </div>
+        )
+    }
+
+}
+
+export default withRouter(TabEstadisticas);

0568c9549acc8536312d0b65dff4766d00ae18fe
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-08-01 21:41:14 -0300

    S5-55 Remuevo constante sin usar

--- mapache-web/src/component/ModuloProyectos/Tareas/Backlog.js
+++ mapache-web/src/component/ModuloProyectos/Tareas/Backlog.js
@@ -73,7 +73,7 @@ class Backlog extends Component {
     }
 
     render() {
-        const proyectoId = +this.props.match.params.id;
+        
         return (
             <div className="proyectos-screen-div">
                  <TablaAdministracion

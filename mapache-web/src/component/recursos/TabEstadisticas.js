175c0a49e44b1480ca4e8d3b16e8b8cbed05ed8b
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-08-03 17:05:57 -0300

    SIDG-73 Se agrega chart para estadisticas anuales

--- mapache-web/src/component/recursos/TabEstadisticas.js
+++ mapache-web/src/component/recursos/TabEstadisticas.js
@@ -76,6 +76,10 @@ class TabEstadisticas extends Component {
             chart = <ChartMensual
                         fechaSeleccionada={ this.state.fechaSeleccionada }
                     ></ChartMensual>
+        } else if (this.state.frecuencia === 3) {
+            chart = <ChartAnual
+                        fechaSeleccionada={ this.state.fechaSeleccionada }
+                    ></ChartAnual>
         }
 
         return (
@@ -449,4 +453,149 @@ class ChartMensual extends Component {
         );
     }
     
+}
+
+class ChartAnual extends Component {
+
+    componentDidMount() {
+        let chart = am4core.create("chart-anual", am4charts.XYChart);
+
+        // Add data
+        chart.data = [{
+            "mes": "Enero",
+            "value": 600000
+            }, {
+            "mes": "Febrero",
+            "value": 900000
+            }, {
+            "mes": "Marzo",
+            "value": 180000
+            }, {
+            "mes": "Abril",
+            "value": 600000
+            }, {
+            "mes": "Mayo",
+            "value": 350000
+            }, {
+            "mes": "Junio",
+            "value": 600000
+            }, {
+            "mes": "Julio",
+            "value": 670000
+            }, {
+                "mes": "Agosto",
+                "value": 600000
+            }, {
+                "mes": "Septiembre",
+                "value": 350000
+            }, {
+                "mes": "Octubre",
+                "value": 600000
+            }, {
+                "mes": "Noviembre",
+                "value": 670000
+            }, {
+                "mes": "Diciembre",
+                "value": 670000
+            }
+        ];
+
+        // Populate data
+        for (var i = 0; i < (chart.data.length - 1); i++) {
+            chart.data[i].valueNext = chart.data[i + 1].value;
+        }
+
+        // Create axes
+        var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+        categoryAxis.dataFields.category = "mes";
+        categoryAxis.renderer.grid.template.location = 0;
+        categoryAxis.renderer.minGridDistance = 30;
+
+        var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+        valueAxis.min = 0;
+
+        // Create series
+        var series = chart.series.push(new am4charts.ColumnSeries());
+        series.dataFields.valueY = "value";
+        series.dataFields.categoryX = "mes";
+
+        // Add series for showing variance arrows
+        var series2 = chart.series.push(new am4charts.ColumnSeries());
+        series2.dataFields.valueY = "valueNext";
+        series2.dataFields.openValueY = "value";
+        series2.dataFields.categoryX = "mes";
+        series2.columns.template.width = 1;
+        series2.fill = am4core.color("#555");
+        series2.stroke = am4core.color("#555");
+
+        // Add a triangle for arrow tip
+        var arrow = series2.bullets.push(new am4core.Triangle);
+        arrow.width = 10;
+        arrow.height = 10;
+        arrow.horizontalCenter = "middle";
+        arrow.verticalCenter = "top";
+        arrow.dy = -1;
+
+        // Set up a rotation adapter which would rotate the triangle if its a negative change
+        arrow.adapter.add("rotation", function(rotation, target) {
+        return getVariancePercent(target.dataItem) < 0 ? 180 : rotation;
+        });
+
+        // Set up a rotation adapter which adjusts Y position
+        arrow.adapter.add("dy", function(dy, target) {
+        return getVariancePercent(target.dataItem) < 0 ? 1 : dy;
+        });
+
+        // Add a label
+        var label = series2.bullets.push(new am4core.Label);
+        label.padding(10, 10, 10, 10);
+        label.text = "";
+        label.fill = am4core.color("#0c0");
+        label.strokeWidth = 0;
+        label.horizontalCenter = "middle";
+        label.verticalCenter = "bottom";
+        label.fontWeight = "bolder";
+
+        // Adapter for label text which calculates change in percent
+        label.adapter.add("textOutput", function(text, target) {
+        var percent = getVariancePercent(target.dataItem);
+        return percent ? percent + "%" : text;
+        });
+
+        // Adapter which shifts the label if it's below the variance column
+        label.adapter.add("verticalCenter", function(center, target) {
+        return getVariancePercent(target.dataItem) < 0 ? "top" : center;
+        });
+
+        // Adapter which changes color of label to red
+        label.adapter.add("fill", function(fill, target) {
+        return getVariancePercent(target.dataItem) < 0 ? am4core.color("#c00") : fill;
+        });
+
+        function getVariancePercent(dataItem) {
+        if (dataItem) {
+            var value = dataItem.valueY;
+            var openValue = dataItem.openValueY;
+            var change = value - openValue;
+            return Math.round(change / openValue * 100);
+        }
+        return 0;
+        }
+
+
+        this.chart = chart;
+    }
+
+    componentWillUnmount() {
+        if (this.chart) {
+            this.chart.dispose();
+        }
+    }
+    
+    render() {
+        return (
+            <div className="chart-anual"></div>
+        );
+    }
+
 }

8e8956555375c31f56feab01c64ce5af4e10c547
Constanza Frutos <constanza.frutos.ramos@gmail.com>
2020-08-03 20:21:17 -0300

    SIDG-73 Otro formado de grafico mensual

--- mapache-web/src/component/recursos/TabEstadisticas.js
+++ mapache-web/src/component/recursos/TabEstadisticas.js
@@ -225,78 +225,7 @@ class ChartSemanal extends Component {
   
         this.chart = chart;
     }
-    /*componentDidMount() {
-        let chart = am4core.create("chart-semanal", am4charts.XYChart);
-
-        // Add data
-        chart.data = [{
-            "year": "2016",
-            "europe": 2.5,
-            "namerica": 2.5,
-            "asia": 2.1,
-            "lamerica": 0.3,
-            "meast": 0.2,
-            "africa": 0.1
-        }, {
-            "year": "2017",
-            "europe": 2.6,
-            "namerica": 2.7,
-            "asia": 2.2,
-            "lamerica": 0.3,
-            "meast": 0.3,
-            "africa": 0.1
-        }, {
-            "year": "2018",
-            "europe": 2.8,
-            "namerica": 2.9,
-            "asia": 2.4,
-            "lamerica": 0.3,
-            "meast": 0.3,
-            "africa": 0.1
-        }];
-        
-        chart.legend = new am4charts.Legend();
-        chart.legend.position = "right";
-        
-        // Create axes
-        var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
-        categoryAxis.dataFields.category = "year";
-        categoryAxis.renderer.grid.template.opacity = 0;
-        
-        var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
-        valueAxis.min = 0;
-        valueAxis.renderer.grid.template.opacity = 0;
-        valueAxis.renderer.ticks.template.strokeOpacity = 0.5;
-        valueAxis.renderer.ticks.template.stroke = am4core.color("#495C43");
-        valueAxis.renderer.ticks.template.length = 10;
-        valueAxis.renderer.line.strokeOpacity = 0.5;
-        valueAxis.renderer.baseGrid.disabled = true;
-        valueAxis.renderer.minGridDistance = 40;
-        
-        // Create series
-        function createSeries(field, name) {
-            var series = chart.series.push(new am4charts.ColumnSeries());
-            series.dataFields.valueX = field;
-            series.dataFields.categoryY = "year";
-            series.stacked = true;
-            series.name = name;
-            
-            var labelBullet = series.bullets.push(new am4charts.LabelBullet());
-            labelBullet.locationX = 0.5;
-            labelBullet.label.text = "{valueX}";
-            labelBullet.label.fill = am4core.color("#fff");
-        }
-        
-        createSeries("europe", "Europe");
-        createSeries("namerica", "North America");
-        createSeries("asia", "Asia");
-        createSeries("lamerica", "Latin America");
-        createSeries("meast", "Middle East");
-        createSeries("africa", "Africa");
-
-        this.chart = chart;
-    }*/
-
+    
     componentWillUnmount() {
         if (this.chart) {
             this.chart.dispose();
@@ -313,7 +242,7 @@ class ChartSemanal extends Component {
 
 class ChartMensual extends Component {
 
-    componentDidMount() {
+    /*componentDidMount() {
         let chart = am4core.create("chart-mensual", am4charts.XYChart);
         chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
 
@@ -459,6 +388,86 @@ class ChartMensual extends Component {
         bullet5.label.fill = am4core.color("#ffffff");
 
         chart.scrollbarX = new am4core.Scrollbar();
+
+        this.chart = chart;
+    }*/
+
+    componentDidMount() {
+        let chart = am4core.create("chart-mensual", am4charts.XYChart);
+
+        // Add data
+        chart.data = [
+            {
+                category: "One",
+                Vacaciones: 0,
+                Enfermedad: 0,
+                DíaDeEstudio: 9,
+                Tarea: 29,
+                NoOcupado: 2
+            },
+            {
+                category: "Two",
+                Vacaciones: 18,
+                Enfermedad: 0,
+                DíaDeEstudio: 0,
+                Tarea: 2,
+                NoOcupado: 2
+            },
+            {
+                category: "Three",
+                Vacaciones: 0,
+                Enfermedad: 0,
+                DíaDeEstudio: 0,
+                Tarea: 40,
+                NoOcupado: 0
+            },
+            {
+                category: "Four",
+                Vacaciones: 0,
+                Enfermedad: 9,
+                DíaDeEstudio: 0,
+                Tarea: 30,
+                NoOcupado: 1
+            }
+        ];
+        
+        chart.legend = new am4charts.Legend();
+        chart.legend.position = "right";
+        
+        // Create axes
+        var categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
+        categoryAxis.dataFields.category = "category";
+        categoryAxis.renderer.grid.template.opacity = 0;
+        
+        var valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
+        valueAxis.min = 0;
+        valueAxis.renderer.grid.template.opacity = 0;
+        valueAxis.renderer.ticks.template.strokeOpacity = 0.5;
+        valueAxis.renderer.ticks.template.stroke = am4core.color("#495C43");
+        valueAxis.renderer.ticks.template.length = 10;
+        valueAxis.renderer.line.strokeOpacity = 0.5;
+        valueAxis.renderer.baseGrid.disabled = true;
+        valueAxis.renderer.minGridDistance = 40;
+        
+        // Create series
+        function createSeries(field, name) {
+            var series = chart.series.push(new am4charts.ColumnSeries());
+            series.dataFields.valueX = field;
+            series.dataFields.categoryY = "category";
+            series.stacked = true;
+            series.name = name;
+            
+            var labelBullet = series.bullets.push(new am4charts.LabelBullet());
+            labelBullet.locationX = 0.5;
+            labelBullet.label.text = "{valueX}";
+            labelBullet.label.fill = am4core.color("#fff");
+        }
+        
+        createSeries("Vacaciones", "Vacaciones");
+        createSeries("Enfermedad", "Enfermedad");
+        createSeries("DiaDeEstudio", "Día de estudio");
+        createSeries("Tarea", "Tarea");
+        createSeries("NoOcupado", "No ocupado");
 
         this.chart = chart;
     }
